48d31ca460843f2072772ebefe1c17c0598e3022
achiniishani1993 <achiniishani1993@gmail.com>
2026-04-30 15:17:01 +0200

    Created EliminatedPage and gave navigate to buttons

--- src/pages/EliminatedPage.jsx
+++ src/pages/EliminatedPage.jsx
@@ -1,9 +1,25 @@
-
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import "../styles/eliminated.css";
 
 const EliminatedPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>EliminatedPage</div>
-  )
-}
+    <div className="eliminated d-flex flex-column justify-content-center align-items-center text-center">
+      <h1 className="fw-bold text-white display-1 display-md-1 display-sm-2">
+        ELIMINATED!
+      </h1>
 
-export default EliminatedPage
+      <div className="d-flex flex-column flex-md-row gap-5 p-4 mt-4 w-100 justify-content-center align-items-center">
+        <Button variant="light" className="big-btn fw-bold" onClick={()=>navigate("/game")}>
+          TRY AGAIN
+        </Button>
+        <Button variant="dark" className="big-btn fw-bold" onClick={()=>navigate("/leaderboard")}>
+          LEADERBOARD
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default EliminatedPage;
