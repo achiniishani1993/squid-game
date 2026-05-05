6c3dc5f3d3e12ced739bf1ca067f997f13d09230
Anitha-Thunga <ani.cvs@gmail.com>
2026-05-05 18:08:41 +0200

    buid LoginPage UI and validation (#2)

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-
+ 
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css"
-import  './styles/global.css'
+import "bootstrap-icons/font/bootstrap-icons.css";
+import './styles/global.css'
 import AppRoutes from './routes/AppRoutes'
-
+import { AuthProvider } from './context/AuthContext';
+ 
 createRoot(document.getElementById('root')).render(
- <BrowserRouter>
-    <AppRoutes />
-   </BrowserRouter>
+   <AuthProvider>
+      <BrowserRouter>
+         <AppRoutes />
+      </BrowserRouter>
+   </AuthProvider>
 )
+ 
+ 
