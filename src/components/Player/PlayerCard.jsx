2d7734129800ae43c2f0797201c4b70edfe5a620
achiniishani1993 <achiniishani1993@gmail.com>
2026-05-06 21:10:35 +0200

    build playerselection component and added bootsrap & css

--- src/components/Player/PlayerCard.jsx
+++ src/components/Player/PlayerCard.jsx
@@ -8,17 +8,46 @@ const PlayerCard = ({ player, isSelected, onSelect }) => {
   if (!player) return null;
 
   return (
-    <div>
-      <Card onClick={() => onSelect(player)} style={{ width: "18rem" }}>
-        <Card.Img
-          variant="top"
-          src={player.img}
-          alt={player.name}
-          style={{ width: "80px", borderRadius: "50%" }}
-        />
-        <Card.Body>
-          <Card.Title> #{player.id}</Card.Title>
-          <Card.Text>{player.name}</Card.Text>
+    <div className="d-flex justify-content-center">
+      <Card
+        onClick={() => onSelect(player)}
+        className="text-center shadow-sm h-100"
+        style={{
+          width: "160px",
+          height: "220px", 
+          cursor: "pointer",
+          border: isSelected ? "2px solid hotpink" : "1px solid #E40166",
+        }}
+      >
+        <div className="d-flex justify-content-center mt-3">
+          <Card.Img
+            src={player.img}
+            alt={player.name}
+            style={{
+              width: "80px",
+              height: "80px",
+              objectFit: "cover", 
+              borderRadius: "50%",
+            }}
+          />
+        </div>
+
+        <Card.Body className="d-flex flex-column justify-content-center align-items-center p-2">
+          <Card.Title className="mb-1 fs-6 rounded "  style={{ backgroundColor: "#E40166" , width:"70px" }}> 
+            #{player.id}
+          </Card.Title>
+
+          <Card.Text
+            className="mb-0"
+            style={{
+              fontSize: "0.9rem",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap", 
+            }}
+          >
+            {player.name}
+          </Card.Text>
         </Card.Body>
       </Card>
     </div>
--- src/components/Player/PlayerSelection.jsx
+++ src/components/Player/PlayerSelection.jsx
@@ -1,15 +1,85 @@
 //Display all selected players
 // Renders the full grid of PlayerCards
 // Owns the "which card is selected" state
-
-import PlayerCard from "./PlayerCard"
+import { useState } from "react";
+import PlayerCard from "./PlayerCard";
+import Button from "react-bootstrap/Button";
+import Logo from "../../assets/images/logo-Pink.png";
+import img456 from "../../assets/images/456.png";
+import img388 from "../../assets/images/388.png";
+import img246 from "../../assets/images/246.png";
+import img120 from "../../assets/images/120.png";
+import img149 from "../../assets/images/149.png";
+import "../../styles/playerSelection.css";
 
 const PlayerSelection = () => {
-  return (
-    <div>
-      <PlayerCard/>
-    </div>
-  )
-}
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
 
-export default PlayerSelection
+  const PLAYERS = [
+    { id: 456, name: "Seong Gi-hun", img: img456 },
+    { id: 388, name: "Cho Sang-woo", img: img388 },
+    { id: 246, name: "Jang Deok-su", img: img246 },
+    { id: 120, name: "Ji-yeong", img: img120 },
+    { id: 149, name: "Kang Sae-byeok", img: img149 },
+  ];
+
+  const handleSelect = () => {
+    setSelectedPlayer(player);
+    // save to the local storage
+    const currentUser = JSON.parse(localStorage.getItem("currentUser")) || {};
+    localStorage.getItem(
+      "currentUser",
+      JSON.stringify({ ...currentUser, character: player }),
+    );
+  };
+
+  return (
+    <div className="d-flex align-items-center justify-content-center py-4">
+      <div
+        className="bg-white text-dark rounded-4 shadow-lg w-100 mx-2 p-2 d-flex flex-column"
+        style={{ maxWidth: "1200px", minHeight: "100vh" }}
+      >
+        <div className="justify-content-center text-center align-items-center">
+          <img
+            src={Logo}
+            alt="Squid Game logo"
+            className="img-fluid mb-3 "
+            style={{ maxWidth: "250px" }}
+          />
+
+          <p className="mb-2 fs-4">
+            "You bet on horses? It's the same here, but we bet on humans. You're
+            our horses."
+          </p>
+
+          <h2 className="fw-bold mb-2">Select your player</h2>
+          {/* need to getItem from localStorage- username */}
+          <p>
+            Welcome,,
+            <span style={{ color: "#E40166", fontWeight: "600" }}>
+              username
+            </span>
+          </p>
+        </div>
+        <div className="d-flex flex-wrap justify-content-center gap-3 mt-4">
+          {PLAYERS.map((player) => (
+            <PlayerCard
+              key={player.id}
+              player={player}
+              isSelected={selectedPlayer?.id === player.id}
+              onSelect={handleSelect}
+            />
+          ))}
+        </div>
+
+        <div className="d-flex flex-column justify-content-center align-items-center gap-3 mt-5">
+          <Button variant="dark" disabled={!selectedPlayer}  className="btn px-4 py-3 fw-bold fs-1 ">PLAY NOW</Button>
+
+          <Button variant="light"  className="btn leaderboard-btn px-4 py-3 fw-bold fs-6">🏆 LEADERBOARD</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlayerSelection;
