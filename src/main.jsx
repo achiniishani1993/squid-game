767c3e7023cdf4d9b0a876be91ca4e9b15996def
Anitha-Thunga <ani.cvs@gmail.com>
2026-05-07 17:49:44 +0200

    deleted AuthContext.jsx and GameContext.jsx and updated main.jsx and LoginPage (#6)

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './styles/global.css'
 import AppRoutes from './routes/AppRoutes'
-import { AuthProvider } from './context/AuthContext';
+
  
 createRoot(document.getElementById('root')).render(
-   <AuthProvider>
+   
       <BrowserRouter>
          <AppRoutes />
       </BrowserRouter>
-   </AuthProvider>
+
 )
  
  
