02138641488d6d1ec7e33dff11911a99c9d2368b
achiniishani1993 <achiniishani1993@gmail.com>
2026-05-07 11:03:35 +0200

    updated comment

--- src/pages/WinPage.jsx
+++ src/pages/WinPage.jsx
@@ -49,7 +49,7 @@ const WinPage = () => {
         />
         <h1 className="fw-bold display-1 display-md-1 display-sm-2">WINNER!</h1>
         <p className="fs-1 fw-bold ">Score: </p>
-        {/* TODO - need to read score from api */}
+        {/* TODO - need to read score from localStorage */}
       </div>
 
       <div className="d-flex flex-column flex-md-row gap-5 p-4 mt-4 w-100 justify-content-center align-items-center">
