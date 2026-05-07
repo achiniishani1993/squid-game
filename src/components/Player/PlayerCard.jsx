2b635b86a98e4784c9020c2237db9d0235008653
achiniishani1993 <achiniishani1993@gmail.com>
2026-05-07 10:47:28 +0200

    moved card body  inline styles to css file

--- src/components/Player/PlayerCard.jsx
+++ src/components/Player/PlayerCard.jsx
@@ -3,6 +3,7 @@
 // Returns:    just the card UI
 
 import Card from "react-bootstrap/Card";
+import "../../styles/playerSelection.css";
 
 const PlayerCard = ({ player, isSelected, onSelect }) => {
   if (!player) return null;
@@ -14,40 +15,30 @@ const PlayerCard = ({ player, isSelected, onSelect }) => {
         className="text-center shadow-sm h-100"
         style={{
           width: "160px",
-          height: "220px", 
+          height: "220px",
           cursor: "pointer",
           border: isSelected ? "3px solid hotpink" : "1px solid #E40166",
         }}
       >
-        <div className="d-flex justify-content-center mt-3">
+        <div className="d-flex justify-content-center mt-3 ">
           <Card.Img
             src={player.img}
             alt={player.name}
             style={{
               width: "80px",
               height: "80px",
-              objectFit: "cover", 
+              objectFit: "cover",
               borderRadius: "50%",
             }}
           />
         </div>
 
         <Card.Body className="d-flex flex-column justify-content-center align-items-center p-2">
-          <Card.Title className="mb-1 fs-6 rounded "  style={{ backgroundColor: "#E40166" , width:"70px" }}> 
+          <Card.Title className="mb-1 fs-6 rounded card-title">
             #{player.id}
           </Card.Title>
 
-          <Card.Text
-            className="mb-0"
-            style={{
-              fontSize: "0.9rem",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              whiteSpace: "nowrap", 
-            }}
-          >
-            {player.name}
-          </Card.Text>
+          <Card.Text className="mb-0 card-text">{player.name}</Card.Text>
         </Card.Body>
       </Card>
     </div>
