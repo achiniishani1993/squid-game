f7233f480e56f73ad3fbee7a62cd5944fe56dcde
achiniishani1993 <achiniishani1993@gmail.com>
2026-05-12 13:57:35 +0200

    save that user LOST in eliminated page-leaderboard

--- src/pages/EliminatedPage.jsx
+++ src/pages/EliminatedPage.jsx
@@ -4,6 +4,16 @@ import "../styles/eliminated.css";
 
 const EliminatedPage = () => {
   const navigate = useNavigate();
+  // before navigating leaderboard update localStorage
+  const handleLeaderboard = () => {
+    const savedUser = JSON.parse(localStorage.getItem("currentUser"));
+
+    localStorage.setItem(
+      "currentUser",
+      JSON.stringify({ ...savedUser, hasPlayed: true, isWinner: false }),
+    );
+    navigate("/leaderboard");
+  };
   return (
     <div className="eliminated d-flex flex-column justify-content-center align-items-center text-center">
       <h1 className="fw-bold text-white display-1 display-md-1 display-sm-2">
@@ -21,7 +31,7 @@ const EliminatedPage = () => {
         <Button
           variant="dark"
           className="big-btn fw-bold"
-          onClick={() => navigate("/leaderboard")}
+          onClick={handleLeaderboard}
         >
           LEADERBOARD
         </Button>
